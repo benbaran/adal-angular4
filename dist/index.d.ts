a957cea5cb229fc5c695a01dff26a3cffe820634
Ben Baran <ben.baran@gmail.com>
2017-04-12 08:49:28 -0500

    Added ADAL4User.

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
+export { ADAL4User } from "./adal4-user";
 export { ADAL4Module } from "./adal4.module";
 export { ADAL4HTTPModule } from "./adal4-http.module";
 export { ADAL4Service } from "./adal4.service";
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+export { ADAL4User } from "./adal4-user";
+
 export { ADAL4Module } from "./adal4.module";
 export { ADAL4HTTPModule } from "./adal4-http.module";
 
