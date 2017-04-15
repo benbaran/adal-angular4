ff117ecb0ee86635e852450c1d12aa4da8158d60
Ben Baran <ben.baran@gmail.com>
2017-04-15 11:34:01 -0500

    Publish new version to npm.

--- scripts/npm-update.ps1
+++ scripts/npm-update.ps1
@@ -12,18 +12,12 @@ npm install --save @angular/platform-browser@latest
 npm install --save @angular/http@latest
 
 # Install ADAL and ADAL Types
-npm install --save @types/adal@latest
 npm install --save adal-angular@latest
 
 # Install Development Tools
 #npm install --save-dev typescript@latest
-#npm install --save-dev eslint@latest
-#npm install --save-dev eslint-config-google@latest
 #npm install --save-dev gulp@latest
 #npm install --save-dev gulp-util@latest
-#npm install --save-dev tslint@latest
-#npm install --save-dev karma@latest
 #npm install --save-dev jasmine-core@latest
-#npm install --save-dev karma-jasmine@latest
 #npm install --save-dev @types/jasmine@latest
 
