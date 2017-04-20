33849d390e58bfb8fe705b8bc3942ff30f25b6dd
Ben Baran <ben.baran+git@gmail.com>
2017-04-20 13:44:44 -0500

    Version 0.0.91 release.

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,3 @@
 export { Adal4User } from './adal4-user';
 export { Adal4Service } from './adal4.service';
+export { Adal4HTTPService } from './adal4-http.service';
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // main export file
 export {Adal4User} from './adal4-user';
 export {Adal4Service} from './adal4.service';
+export {Adal4HTTPService} from './adal4-http.service';
 
 
 
