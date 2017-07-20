824a03e62d230eba362e8f0b9ffc4461dbb0d001
Ben Baran <ben.baran@gmail.com>
2017-07-20 14:00:41 -0500

    Version 1.1.10 release.

--- dist/adal4.service.js
+++ dist/adal4.service.js
@@ -8,6 +8,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var rxjs_1 = require("rxjs");
+var adalLib = require("adal-angular");
 /**
  *
  *
