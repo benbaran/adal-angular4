070a1ef7aa1ade590fce8a25c0c2ec05adc995b3
Giulio Roman <giulio.roman@eurac.edu>
2018-03-06 12:51:19 +0100

    fix AdalInterceptor; improve gulpfile
    The AdalInterceptor was not working correctly: it always injected the
    wrong token to the HTTP request. Instead of just using the token stored
    into `this.adalService.userInfo.token`, the interceptor should:
    
    1. determine which endpoint the request is directed to (with respect
       to `environment.adalConfig.endpoints`
    2. if the request is not directed to a registered endpoint, then it
       should pass the request to the next handler
    3. if instead the request is directed to a registered endpoint, the
       interceptor should acquire the token for that specific endpoint
    4. and inject it into the request before passing it to the next handler
    
    Some improvements to the gulpfile have also been implemented.

--- adal.interceptor.ts
+++ adal.interceptor.ts
@@ -1,18 +1,27 @@
 import { Injectable, Injector } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { AdalService } from './adal.service';
-
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AdalInterceptor implements HttpInterceptor {
 
-    constructor(private adalService: AdalService) { }
+    constructor(private adal: AdalService) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-        const authReq = req.clone({ headers: req.headers.set('Authorization', `Bearer ${this.adalService.userInfo.token}`) });
-
-        return next.handle(authReq);
+        const url = req.url;
+        const resource = this.adal.GetResourceForEndpoint(url);
+        if (resource && this.adal.userInfo.authenticated) {
+            let headers = req.headers || new HttpHeaders();
+            this.adal.acquireToken(resource)
+                .subscribe((token: string) => {
+                    headers = headers.append('Authorization', 'Bearer ' + token);
+                }
+            );
+            return next.handle(req.clone({ headers: headers }));        
+        } else {
+            return next.handle(req.clone());
+        }
     }
 }
--- gulpfile.js
+++ gulpfile.js
@@ -6,42 +6,43 @@ var
     replace = require('gulp-replace'),
     fs = require('fs');
 
+// delete ./dist directory
 gulp.task('clean', function () {
     del(['./dist/*']);
 });
 
-gulp.task('copy', function (cb) {
-    gulp.src(['adal-angular.d.ts']).pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('copy-package', function (cb) {
-
-    const pkgjson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
-
-    delete pkgjson.scripts;
-
-    delete pkgjson.devDependencies;
-
-    const filepath = './dist/package.json';
-
-    fs.writeFileSync(filepath, JSON.stringify(pkgjson, null, 2), 'utf-8');
-});
-
-gulp.task('replace', function () {
-    gulp.src(['./dist/adal.service.d.ts'])
-        .pipe(replace('../adal-angular.d.ts', './adal-angular.d.ts'))
-        .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('build', function (cb) {
-    exec('npm run build', function (err, stdout, stderr) {
+// execute npm compile script
+gulp.task('compile', ['clean'], function (cb) {
+    exec('npm run compile', function (err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
         cb(err);
     });
 });
 
-gulp.task('bump', function () {
+// include package.json file in ./dist folder
+gulp.task('package', ['compile'], function () {
+    const pkgjson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+    delete pkgjson.scripts;
+    delete pkgjson.devDependencies;
+    const filepath = './dist/package.json';
+    fs.writeFileSync(filepath, JSON.stringify(pkgjson, null, 2), 'utf-8');
+});
+
+// include type definition file for adal-angular
+gulp.task('copy', ['package'], function () {
+    gulp.src(['adal-angular.d.ts']).pipe(gulp.dest('./dist/'));
+})
+
+// rewrite type definition file path for adal-angular in adal.service.d.ts
+gulp.task('replace', ['copy'], function () {
+    gulp.src('./dist/adal.service.d.ts')
+    .pipe(replace('../adal-angular.d.ts', './adal-angular.d.ts'))
+    .pipe(gulp.dest('./dist/'))
+})
+
+// increase the version in package.json
+gulp.task('bump', ['replace'], function () {
     gulp.src('./package.json')
         .pipe(bump({
             type: 'patch'
@@ -49,7 +50,8 @@ gulp.task('bump', function () {
         .pipe(gulp.dest('./'));
 });
 
-gulp.task('git-add', function (cb) {
+// git add
+gulp.task('git-add', ['bump'], function (cb) {
     exec('git add -A', function (err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
@@ -57,11 +59,9 @@ gulp.task('git-add', function (cb) {
     });
 });
 
-
-gulp.task('git-commit', function (cb) {
-
+// git commit
+gulp.task('git-commit', ['git-add'], function (cb) {
     var package = require('./package.json');
-
     exec('git commit -m "Version ' + package.version + ' release."', function (err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
@@ -69,8 +69,8 @@ gulp.task('git-commit', function (cb) {
     });
 });
 
-gulp.task('git-push', function (cb) {
-
+// git push
+gulp.task('git-push', ['git-commit'], function (cb) {
     exec('git push', function (err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
@@ -78,11 +78,14 @@ gulp.task('git-push', function (cb) {
     });
 });
 
-gulp.task('publish', function (cb) {
-
+// publish ./dist directory to npm
+gulp.task('publish', ['git-push'], function (cb) {
     exec('npm publish ./dist', function (err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
         cb(err);
     });
 });
+
+// build ./dist folder - used for development
+gulp.task('default', ['replace'], function (cb) {});
