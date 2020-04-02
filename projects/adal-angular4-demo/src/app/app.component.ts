9322a1687cc1bc1fefa9999275b455f2a05b426d
Benjamin Baran <benjamin.baran@accenture.com>
2020-04-02 10:57:40 -0500

    Updated to Angular 9

---
+++ projects/adal-angular4-demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'demo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'adal-angular4-demo';
+}
---
+++ projects/adal-angular4/src/public-api.ts
@@ -0,0 +1,5 @@
+
+export * from './lib/adal.module';
+export * from './lib/adal.guard';
+export * from './lib/adal.service';
+export * from './lib/adal.interceptor';
